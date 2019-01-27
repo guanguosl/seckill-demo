bda1510a208c0236f38a49249407cf1763c474ca
guanyl-pc <guanyl@servyou.com.cn>
2019-01-27 14:15:50 +0800

    add vo

--- src/main/java/com/imooc/gsl/vo/LoginVO.java
+++ src/main/java/com/imooc/gsl/vo/LoginVO.java
@@ -2,7 +2,7 @@ package com.imooc.gsl.vo;
 
 import java.io.Serializable;
 
-public class LoginVO implements Serializable {
+public class LoginVo implements Serializable {
     private String mobile;
     private String password;
 
